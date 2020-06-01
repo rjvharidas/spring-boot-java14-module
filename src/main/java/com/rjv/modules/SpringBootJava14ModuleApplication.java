4a6e0c8620d5c5d0b727ae688d0b5f89df5d4a93
Rajeev <rjv.haridas@gmail.com>
2020-06-01 15:54:00 +0530

    initial commit

---
+++ src/main/java/com/rjv/modules/SpringBootJava14ModuleApplication.java
@@ -0,0 +1,13 @@
+package com.rjv.modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJava14ModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJava14ModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rjv/modules/SpringBootJava14ModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjv.modules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJava14ModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
